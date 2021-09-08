048ea3ae436347cce33e569e1d1c0e716ded10b6
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-07 18:49:08 -0600

    Test: Refactor #initialize

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,6 +9,13 @@ describe Player do
 
       expect(player).to be_an_instance_of(Player)
     end
+
+    it "has readable attributes" do
+      player = Player.new({name: "Luca Modric", position: "midfielder"})
+
+      expect(player.name).to eq("Luca Modric")
+      expect(player.position).to eq("midfielder")
+    end
   end
 
 end
