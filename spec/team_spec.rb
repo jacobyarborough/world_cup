cbc4df0455fdd62ba51efec83235894367b13d28
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-07 19:06:11 -0600

    Add to #initialize test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -6,9 +6,15 @@ describe Team do
 
   describe '#initialize' do
     it 'creates an instance object for class Team' do
-      team = Team.new
+      team = Team.new('France')
 
       expect(team).to be_an_instance_of(Team)
+    end
+
+    it 'to have readable attributes' do
+      team = Team.new('France')
+
+      expect(team.country).to eq('France')
     end
   end
 
